1ce06f588e134840bd3d9d2b008dee68e915995c
blackndoor <blackndoor@gmail.com>
2017-05-06 16:30:29 +0200

    solution for crackme64

---
+++ crackme_argv_basic/solve64.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# python -W ignore solve.py
+
+import angr, monkeyhex, claripy
+
+# load the file
+project = angr.Project('crackme64')
+
+# print info from the binary
+print "[i] Architecture    : "+str(project.arch)
+print "[i] Entry point     : "+hex(project.entry)
+print "[i] Loader          : "+str(project.loader)
+print "[i] Main bin object : "+str(project.loader.main_bin)
+
+# set the argv1
+# as I don't know the len, I symbolize 100 bits
+argv1 = angr.claripy.BVS("argv1", 100*8)
+
+# create a new symbolic state
+state = project.factory.path(args=["./crackme64",argv1])
+
+# start the symbolic execution, create a PathGroup
+group = project.factory.path_group(state)
+
+# explore to the "well done" state (0x4005dc found with IDA)
+# avoiding the "get out" state (0x4005e8 found with IDA)
+group.explore(find=0x4005dc, avoid=0x4005e8)
+
+# select the first solution
+found = group.found[0]
+
+# ask to the symbolic solver to get the value of argv1 in the reached state
+solution = found.state.se.any_str(argv1)
+
+# get only the string
+solution = solution[:solution.find("\x00")]
+print "[!] Found : '"+solution+"'"
